450c30becb11ea814047368542cb262a1f2ea8f3
wanghep <53277985@qq.com>
2014-12-10 11:34:19 +0800

    bug fix
    删除数据库的所有数据 bug

--- ZhaoDaoNi/src/com/adn/db/DBManager.java
+++ ZhaoDaoNi/src/com/adn/db/DBManager.java
@@ -97,7 +97,7 @@ public class DBManager {
      * Çå¿ÕÊý¾Ý
      */
     public void clearData() {
-        ExecSQL("DELETE FROM info");
+        ExecSQL("DELETE * FROM info");
         Log.i(TAG, "clear data");
     }
 
